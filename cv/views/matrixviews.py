3e84ff91bc8dec250745a74e60d34226f2b8f2ed
Leon <ho.leon@gmail.com>
2013-02-19 18:52:05 +0100

    Cosmetic changes to matrix list. Renamed template file

--- cv/views/matrixviews.py
+++ cv/views/matrixviews.py
@@ -43,7 +43,7 @@ def matrix_load(request, m_id):
 
 def matrix_load_bb(request, m_id):
 	m = get_object_or_404(Matrix, pk=m_id)
-	return render_to_response('competence/editmatrix_bb.html', {'m':m}, context_instance=RequestContext(request))	
+	return render_to_response('competence/matrix_edit_bb.html', {'m':m}, context_instance=RequestContext(request))	
 
 def addfield(fieldtype, num, readonly="", groupnum="", title="", description="", existing_id = "", nestedfields=False):
     template = 'competence/groupcompetence.html'
