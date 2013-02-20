d472ecf3fa178d8aab2f08da2f9152ee9cae6cd1
Leon <ho.leon@gmail.com>
2013-02-20 03:23:49 +0100

    Fix import bug

--- settings.py
+++ settings.py
@@ -162,7 +162,8 @@ LOGGING = {
 # Webodt-stuff
 WEBODT_CONVERTER = 'webodt.converters.googledocs.GoogleDocsODFConverter'
 WEBODT_GOOGLEDOCS_EMAIL = 'documents.in.space@gmail.com'
-from password import WEBODT_GOOGLEDOCS_PASSWORD
+import password 
+WEBODT_GOOGLEDOCS_PASSWORD = password.WEBODT_GOOGLEDOCS_PASSWORD
 WEBODT_TEMPLATE_PATH = '/var/tmp/'
 WEBODT_CACHE_DIR = WEBODT_TEMPLATE_PATH
 
