ffbe88fae74769244d4abd4b66640ded2671672e
Leon <ho.leon@gmail.com>
2013-02-26 10:27:31 +0100

    Local searchindexes changes - to be removed

--- cv/search_indexes.py
+++ cv/search_indexes.py
@@ -2,12 +2,20 @@ import datetime
 from haystack import indexes
 from cv.models import Person
 
-class JobIndex(indexes.SearchIndex, indexes.Indexable):
+class PersonIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     name = indexes.CharField(model_attr='name', faceted=True)
+    technologies = indexes.MultiValueField()
  
     def get_model(self):
         return Person
  
     def index_queryset(self,using=None):
         return self.get_model().objects.all()
+
+    def prepare_technologies(self, obj):
+    	result = []
+    	for t in obj.technology_set.all():
+    		for tdata in t.data_as_list():
+ 				result.append( tdata.encode('ascii', 'ignore') )
+    	return result
