6505ba73a136a321884ec992b746928909a9bbc4
Leon <ho.leon@gmail.com>
2013-02-27 12:28:21 +0100

    Fixed permanent directory structure

--- schema_gen.sh
+++ schema_gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py build_solr_schema > schema.xml.bli
-sudo mv schema.xml.bli ../solr/solr/collection1/conf/schema.xml
-sudo chown solr:solr ../solr/solr/collection1/conf/schema.xml
+sudo mv schema.xml.bli /home/solr/solr/collection1/conf/schema.xml
+sudo chown solr:solr /home/solr/solr/collection1/conf/schema.xml
