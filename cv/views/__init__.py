3d90b1d9f71c785a2f1f312ff97a1074511f1ccd
cvapp <cvapp@acs02.(none)>
2013-11-19 11:24:54 +0000

    Created subfolder for Django files.

--- cv/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from cv.views.cvviews import *
-from cv.views.matrixviews import *
-from cv.views.matrixentryviews import *
-from cv.views.cvdownload import download, multicv
-from cv.views.loginout import mylogin, myRemoteLogin
