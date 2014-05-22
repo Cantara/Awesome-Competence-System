09cc625951e049db4b0d1f78796a93b4579de77f
Leon <cvapp@acs02.(none)>
2014-05-22 12:22:37 +0000

    Fix for url in reports-view

--- cvapp/cv/views/reports.py
+++ cvapp/cv/views/reports.py
@@ -2,9 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 import csv
+import localsettings
 
 def reports(request):
-	return render_to_response('reports/reports.html', context_instance=RequestContext(request))
+	try:
+		solrurl = localsettings.SOLRURL
+	except:
+		solrurl = "/solr/collection1/select"
+	return render_to_response('reports/reports.html', {'solrurl': solrurl}, context_instance=RequestContext(request))
 
 def reports_download(request):
 	if(request.method == 'POST' and request.POST['data']):
